a4d3328a39f1e316ec0c0b3f5f42d70abae1040a
vinsko <vincent.pattinama@gmail.com>
2024-09-17 20:16:24 +0200

    created temporary mock for selecting the characters

--- Assets/Scenes/NPCSelect/Scripts/SelectionManager.cs
+++ Assets/Scenes/NPCSelect/Scripts/SelectionManager.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 public class SelectionManager : MonoBehaviour
 {
     [SerializeField] private GameObject selectionOption;
+
+    // kan vgm wel weg maar ik laat het ff staan
+    public GameObject[] characterTransforms;
+    
+    public GameObject parent;
     
     private void Start()
     {
@@ -17,23 +22,38 @@ public class SelectionManager : MonoBehaviour
         // get number of total characters from Gamemanager
         var gm = FindObjectOfType<GameManager>();
 
+        int counter = 0;
         foreach (Character c in gm.currentCharacters)
         {
+            //float width = characterspace.rect.width;
+            //float height = characterspace.rect.height;
+            
             // TODO: give proper transform?
             // TODO: make child of SelectionSpace, so it fits in there correctly
-            GameObject newOption = Instantiate(selectionOption, transform);
+            GameObject newOption = Instantiate(selectionOption);
             newOption.GetComponent<SelectOption>().characterId = c.id;
             newOption.GetComponent<SelectOption>().avatar.sprite = c.avatar;
-            newOption.transform.parent =
-                FindObjectOfType<Canvas>().transform.GetChild(1).transform.GetChild(1).transform;
-            // FindObjectOfType<SelectionSpace>().transform;
+                
+            
+            // not correct yet, this will go out of bounds when there are more than 8 characters.
+            // sets one of the 8 characterspaces as parent of the selectoption object.
+            newOption.transform.parent = parent.transform.GetChild(counter);
+            // sets the position of the selectoption object to the same position as the parent (characterspace)
+            newOption.transform.position = newOption.transform.parent.position;
+            counter++;
+
+            
         }
     }
     
-    public void ButtonClicked(int id)
+    
+    public void ButtonClicked(GameObject option)
     {
+        // get the selectoption object
+        SelectOption character = option.GetComponentInChildren<SelectOption>();
+        
         // TODO: ensure that the correct id is passed based on the button
         var gm = FindObjectOfType<GameManager>();
-        gm.StartDialogue(id);
+        gm.StartDialogue(character.characterId);
     }
 }
