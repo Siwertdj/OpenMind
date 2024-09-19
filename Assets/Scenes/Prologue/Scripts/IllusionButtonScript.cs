279017db272e2b1947f2bc33fc03dde96001b9a5
deniseadrianapatricia <denisederijcke@live.nl>
2024-09-19 15:13:05 +0200

    onclick function

--- Assets/Scenes/Prologue/Scripts/IllusionButtonScript.cs
+++ Assets/Scenes/Prologue/Scripts/IllusionButtonScript.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class IllusionButtonScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnClick()
     {
-        
+        //
+        //
     }
 
     // Update is called once per frame
