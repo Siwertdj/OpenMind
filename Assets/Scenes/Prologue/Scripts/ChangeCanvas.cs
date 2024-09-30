8ccfaf5d92f70518b427fea75ffceb3e7bd992d9
Denise de Rijcke <denisederijcke@live.nl>
2024-09-30 17:51:33 +0200

    Adapted prologue code to style guide.

---
+++ Assets/Scenes/Prologue/Scripts/ChangeCanvas.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class CanvasSwitcher : MonoBehaviour
+{
+    // Array that stores all the canvases that are part of the prologue. 
+    public GameObject[] Canvases;
+    
+    /// <summary>
+    /// This method switches between canvases. Each canvas of the array canvases is turned
+    /// off, except for the canvas at location index. 
+    /// </summary>
+    /// <param name="index"></param>
+    public void SwitchCanvas(int index)
+    {
+        foreach (GameObject canvas in Canvases)
+        {
+            canvas.SetActive(false);
+        }
+        Canvases[index].SetActive(true);
+    }
+
+}
