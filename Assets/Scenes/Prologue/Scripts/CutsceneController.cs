59776bdacd3f3f49a2e6bcb52f324c421f1fd4f6
Denise de Rijcke <denisederijcke@live.nl>
2024-10-03 16:17:05 +0200

    Including toggler

--- Assets/Scenes/Prologue/Scripts/CutsceneController.cs
+++ Assets/Scenes/Prologue/Scripts/CutsceneController.cs
@@ -1,17 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.UI;
+
 public class CutsceneController : MonoBehaviour
 {
     public PlayableDirector playableDirector;
+    public GameObject imageWithGrid;                 
+    public GameObject imageWithoutGrid;
+    public Toggle imageToggler;
+
+    private void Start()
+    {
+        imageToggler.onValueChanged.AddListener(OnToggleValueChanged);
+        imageToggler.gameObject.SetActive(false);
+    }
+
     public void PauseTimeline()
     {
         playableDirector.Pause();
+        imageToggler.gameObject.SetActive(true);
     }
 
     public void ContinueTimeline()
     {
         playableDirector.Play();
+    }
+    
+    public void OnToggleValueChanged(bool isOn)
+    {
+        imageWithoutGrid.SetActive(!isOn);
+        imageWithGrid.SetActive(isOn);
     }
 }
