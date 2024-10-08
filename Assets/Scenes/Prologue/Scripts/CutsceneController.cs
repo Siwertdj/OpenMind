b1754300d95e7254bcfcdfe263a8fc1c7c2ad332
Denise de Rijcke <denisederijcke@live.nl>
2024-10-08 10:25:05 +0200

    Fixed index out of range bug

--- Assets/Scenes/Prologue/Scripts/CutsceneController.cs
+++ Assets/Scenes/Prologue/Scripts/CutsceneController.cs
@@ -13,6 +13,7 @@ public class CutsceneController : MonoBehaviour
     public GameObject imageWithGrid;                 
     public GameObject imageWithoutGrid;
     public Toggle imageToggler;
+    public Button continueButton; 
     private void Start()
     {
         //imageToggler.onValueChanged.AddListener(OnToggleValueChanged);
@@ -22,11 +23,18 @@ public class CutsceneController : MonoBehaviour
     public void PauseTimeline()
     {
         playableDirector.Pause();
-        imageToggler.gameObject.SetActive(true);
+        continueButton.gameObject.SetActive(true);
+        //imageToggler.gameObject.SetActive(true);
     }
 
     public void ContinueTimeline()
     {
+        // Make sure both images are removed from the screen. 
+        imageWithoutGrid.SetActive(false);
+        imageWithGrid.SetActive(false);
+        // Disable button
+        continueButton.gameObject.SetActive(false);
+        // Resume timeline.
         playableDirector.Play();
     }
     
