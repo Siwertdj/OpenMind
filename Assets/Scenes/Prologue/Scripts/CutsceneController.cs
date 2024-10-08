09c9e554f06cdcd477a9b37bb5b3a07d22116a09
Denise de Rijcke <denisederijcke@live.nl>
2024-10-08 11:27:53 +0200

    Checkpoint: fixed toggler checkbox bug

--- Assets/Scenes/Prologue/Scripts/CutsceneController.cs
+++ Assets/Scenes/Prologue/Scripts/CutsceneController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine.Events;
 
 public class CutsceneController : MonoBehaviour
@@ -13,18 +14,19 @@ public class CutsceneController : MonoBehaviour
     public GameObject imageWithGrid;                 
     public GameObject imageWithoutGrid;
     public Toggle imageToggler;
-    public Button continueButton; 
+    public Button continueButton;
+    private Transform checkmarkTransform; 
+    
     private void Start()
     {
-        //imageToggler.onValueChanged.AddListener(OnToggleValueChanged);
-       //imageToggler.gameObject.SetActive(false);
+        // Access the Checkmark GameObject via the Toggle's hierarchy
+       checkmarkTransform = imageToggler.transform.Find("Background/Checkmark");
     }
 
     public void PauseTimeline()
     {
         playableDirector.Pause();
         continueButton.gameObject.SetActive(true);
-        //continueButton.interactable = true; 
     }
 
     public void ContinueTimeline()
@@ -35,13 +37,14 @@ public class CutsceneController : MonoBehaviour
         // Disable button
         continueButton.gameObject.SetActive(false);
         // Resume timeline.
-        Debug.Log("button is pressed");
         playableDirector.Play();
     }
     
     public void OnToggleValueChanged(bool isOn)
     {
         imageWithoutGrid.SetActive(!isOn);
+        imageToggler.isOn = isOn; 
+        checkmarkTransform.GameObject().SetActive(isOn);
         imageWithGrid.SetActive(isOn);
     }
     
