c3a92073fc3f1074d6c57e010156368615810c9d
Denise de Rijcke <denisederijcke@live.nl>
2024-10-15 15:15:41 +0200

    First version of story A introduction

--- Assets/Scenes/Prologue/Scripts/TimelineManager.cs
+++ Assets/Scenes/Prologue/Scripts/TimelineManager.cs
@@ -3,19 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.UI;
 
 public class TimelineManager : MonoBehaviour
 {
     public PlayableDirector introStoryA;
     public PlayableDirector introStoryB;
     public PlayableDirector introStoryC;
+    public Button continueButton;
+    
+    private PlayableDirector currentTimeline; 
 
     private void Start()
     {
         // depending on the chosen storyline, play the intro to the story
-        introStoryA.Play();
+        currentTimeline = introStoryA; 
+        currentTimeline.Play();
     }
-    
+
+    public void PauseCurrentTimeline()
+    {
+        currentTimeline.Pause();
+        continueButton.gameObject.SetActive(true);
+    }
+
+    public void ContinueCurrentTimeline()
+    {
+        currentTimeline.Play();
+        continueButton.gameObject.SetActive(false);
+    }
+
     public void StartGame()
     {
         //Debug.Log("intro end");
