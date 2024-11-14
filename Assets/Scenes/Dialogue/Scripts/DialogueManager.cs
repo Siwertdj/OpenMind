0782dff97685fdd551f78cd1fef3db3ae7f7b31a
Siwertdj <57802029+Siwertdj@users.noreply.github.com>
2024-11-14 11:46:09 +0100

    Revert "Feature/207 audiomixer"

--- Assets/Scenes/Dialogue/Scripts/DialogueManager.cs
+++ Assets/Scenes/Dialogue/Scripts/DialogueManager.cs
@@ -43,7 +43,7 @@ public class DialogueManager : MonoBehaviour
     /// and element 1 is the starting dialogue object.</param>
     public void StartDialogue(Component sender, params object[] data)
     {
-        // Set static DialogueManager sm
+        // Set static DialogueManager instance
         dm = this;
 
         // Retrieve and set the dialogue object
