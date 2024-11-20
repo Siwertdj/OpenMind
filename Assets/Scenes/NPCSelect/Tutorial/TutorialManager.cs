3e2e2404082141e2c9582454b940d7833d4c8bef
Hille Brands <hilleannebrands@gmail.com>
2024-11-20 12:50:24 +0100

    Buildable, but no sound on GameButton

--- Assets/Scenes/NPCSelect/Tutorial/TutorialManager.cs
+++ Assets/Scenes/NPCSelect/Tutorial/TutorialManager.cs
@@ -1,7 +1,6 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Build;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.UI;
