4cdc0db330cafd5554bfaefbbc100a6ed674f23b
JoostKootstra <joost.kootstra@gmail.com>
2024-11-24 12:13:14 +0100

    emptied docs folder to test dev documentation

--- docs/search/classes_1.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['characterdata_0',['CharacterData',['../classCharacterData.html',1,'']]],
-  ['characterinstance_1',['CharacterInstance',['../classCharacterInstance.html',1,'']]],
-  ['characterinstanceedittest_2',['CharacterInstanceEditTest',['../classCharacterInstanceEditTest.html',1,'']]],
-  ['customgameevent_3',['CustomGameEvent',['../classCustomGameEvent.html',1,'']]],
-  ['cutscenecontroller_4',['CutsceneController',['../classCutsceneController.html',1,'']]]
-];
--- docs/search/variables_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['i_0',['i',['../classTransitionAnimator.html#a077d633b28fe43071af169dfcf651ed6',1,'TransitionAnimator']]],
-  ['ignores_1',['ignores',['../classDebugManager.html#a9611fbb21181dad98134a3cf63b22089',1,'DebugManager']]]
-];
