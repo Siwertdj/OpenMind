f33d40ddb9264e851242ca9f72a64a6c9765f47e
Seb115 <93035359+Seb115@users.noreply.github.com>
2024-11-28 12:56:09 +0100

    Functionality complete

--- Assets/Scenes/Menus/Scripts/MultiplayerMenuManager.cs
+++ Assets/Scenes/Menus/Scripts/MultiplayerMenuManager.cs
@@ -1,6 +1,10 @@
 // This program has been developed by students from the bachelor Computer Science at Utrecht University within the Software Project course.
 // © Copyright Utrecht University (Department of Information and Computing Sciences)
+
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MultiplayerMenuManager : MonoBehaviour
@@ -9,24 +13,16 @@ public class MultiplayerMenuManager : MonoBehaviour
     [SerializeField] private GameObject multiplayerCanvas;
     [SerializeField] private GameObject hostCanvas;
     [SerializeField] private GameObject joinCanvas;
+    [SerializeField] private GameObject storyCanvas;
 
-    /// <summary>
-    /// Activates the buttons for hosting a game.
-    /// </summary>
-    public void OpenHostMenu()
-    {
-        multiplayerCanvas.SetActive(false);
-        hostCanvas.SetActive(true);
-    }
 
-    /// <summary>
-    /// Activates the buttons for joining a game.
-    /// </summary>
-    public void OpenJoinMenu()
-    {
-        multiplayerCanvas.SetActive(false);
-        joinCanvas.SetActive(true);
-    }
+    [Header("Paramaters")] 
+    [SerializeField] private float maxPlayers = 10;
+    [SerializeField] private TextMeshProUGUI maxPlayersText;
+
+
+    private string classCode;
+    
 
     /// <summary>
     /// Opens the settings menu.
@@ -43,17 +39,104 @@ public class MultiplayerMenuManager : MonoBehaviour
     {
         SceneManager.UnloadSceneAsync("MultiplayerScreenScene");
     }
-
+    
+    /// <summary>
+    /// Goes back to the main multiplayer menu from the host or join menu.
+    /// </summary>
     public void BackToMultiplayer()
     {
         hostCanvas.SetActive(false);
         joinCanvas.SetActive(false);
         multiplayerCanvas.SetActive(true);
     }
-    
-    public void BackToMultiplayerFromJoin()
+
+
+    #region Host
+
+    /// <summary>
+    /// Activates the buttons for hosting a game.
+    /// </summary>
+    public void OpenHostMenu()
     {
-        joinCanvas.SetActive(false);
-        multiplayerCanvas.SetActive(true);
+        multiplayerCanvas.SetActive(false);
+        hostCanvas.SetActive(true);
+    }
+    
+    /// <summary>
+    /// Sets the max players a session can have using a slider.
+    /// </summary>
+    /// <param name="num">The value of the slider.</param>
+    public void SetMaxPlayers(float num)
+    {
+        maxPlayers = num;
+    }
+
+    /// <summary>
+    /// Starts the game as the host.
+    /// The host has to choose a story first
+    /// </summary>
+    public void StartAsHost()
+    {
+        hostCanvas.SetActive(false);
+        storyCanvas.SetActive(true);
+    }
+    
+    //TODO: the following methods are used to start the game as host
+    //TODO: Implement when networking is ready
+    
+    public void StoryA()
+    {
+        
+    }
+
+    public void StoryB()
+    {
+        
+    }
+
+    public void StoryC()
+    {
+        
+    }
+
+    #endregion
+
+
+    #region Join
+
+    /// <summary>
+    /// Activates the buttons for joining a game.
+    /// </summary>
+    public void OpenJoinMenu()
+    {
+        multiplayerCanvas.SetActive(false);
+        joinCanvas.SetActive(true);
+    }
+
+    /// <summary>
+    /// Sets the class code to the input from the player.
+    /// </summary>
+    /// <param name="code">The contents of the input field.</param>
+    public void SetCode(string code)
+    {
+        classCode = code;
+    }
+
+    // TODO: this is where the player joins a game using a class code
+    // TODO: implement when networking is ready
+    /// <summary>
+    /// 
+    /// </summary>
+    public void JoinGame()
+    {
+        
+    }
+    
+
+    #endregion
+    
+    public void Update()
+    {
+        maxPlayersText.text = maxPlayers.ToString();
     }
 }
