a70a63a8b947153d020ee4e1364be9f3183cd484
Hilliekopter <hilleannebrands@gmail.com>
2024-12-24 17:41:45 +0100

    fixed CharacterIcon scaling
    Removed the WaitForEndOfFrame coroutine as it made things a bit ugly

--- Assets/CharacterIcon.cs
+++ Assets/CharacterIcon.cs
@@ -8,30 +8,51 @@ using UnityEngine.TextCore.Text;
 
 public class CharacterIcon : MonoBehaviour
 {
+    private const float ZOOM_FACTOR = 1.8f;
+
     [SerializeField] private Image avatarImageRef;
 
+    private CharacterInstance character;
+
     public void SetAvatar(CharacterInstance character)
-    {        
+    {
+        this.character = character;
+
         // Set the correct sprite
         avatarImageRef.sprite = character.avatarEmotions.Where(es => es.Item1 == Emotion.Neutral).First().Item2;
 
-        // Wait for layout to be initialized before setting scale & pos
-        StartCoroutine(SetSizeAndPosition(character));
+        // Set the image location to the center of the face
+        var rectTransform = avatarImageRef.GetComponent<RectTransform>();
+        rectTransform.pivot = character.data.facePivot;
+        rectTransform.localPosition = Vector2.zero;
+
+        SetAvatarSize();
     }
 
-    private IEnumerator SetSizeAndPosition(CharacterInstance character)
+    private void OnRectTransformDimensionsChange()
     {
-        yield return new WaitForEndOfFrame();
+        // Check edge case
+        if (character == null) 
+            return;
 
-        var rectTransform = avatarImageRef.GetComponent<RectTransform>();
+        // Rescale the avatar if the icon scale has changed
+        SetAvatarSize();
+    }
 
-        // Set the correct size
-        float width = 1.8f * Mathf.Abs(GetComponent<RectTransform>().rect.height);
-        float height = width / (character.avatarEmotions.Where(es => es.Item1 == Emotion.Neutral).First().Item2.rect.width / character.avatarEmotions.Where(es => es.Item1 == Emotion.Neutral).First().Item2.rect.height);
-        rectTransform.sizeDelta = new Vector2(width, height);
+    /// <summary>
+    /// Set the size of the avatar to match the size of the icon.
+    /// </summary>
+    private void SetAvatarSize()
+    {
+        // The ratio between the width & height of the character's sprite
+        float ratio = character.avatarEmotions.Where(
+            es => es.Item1 == Emotion.Neutral).First().Item2.rect.width /
+            character.avatarEmotions.Where(
+            es => es.Item1 == Emotion.Neutral).First().Item2.rect.height;
 
-        // Set the image location to the center of the face
-        rectTransform.pivot = character.data.facePivot;
-        rectTransform.localPosition = new Vector2(0, 0);
+        // Set the avatar size according to the icon's size
+        float width = ZOOM_FACTOR * Mathf.Abs(GetComponent<RectTransform>().rect.height);
+        float height = width / ratio;
+        avatarImageRef.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
     }
 }
