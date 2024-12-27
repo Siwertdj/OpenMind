4f8b3dd7f7464832757e9ee04bbf9456e947d894
JoostKootstra <joost.kootstra@gmail.com>
2024-12-27 14:20:51 +0100

    added buttons to go to main menu in the game menu and story select.

--- Assets/Scenes/Menus/Scripts/StorySelectionManager.cs
+++ Assets/Scenes/Menus/Scripts/StorySelectionManager.cs
@@ -1,4 +1,4 @@
-// This program has been developed by students from the bachelor Computer Science at Utrecht University within the Software Project course.
+﻿// This program has been developed by students from the bachelor Computer Science at Utrecht University within the Software Project course.
 // © Copyright Utrecht University (Department of Information and Computing Sciences)
 using System.Collections;
 using UnityEngine;
@@ -38,6 +38,14 @@ public class StorySelectionManager : MonoBehaviour
     public void StoryCSelected()
     {
         StartIntro(2);
+    }
+
+    /// <summary>
+    /// Back to main menu (Start Screen)
+    /// </summary>
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene("StartScreenScene", LoadSceneMode.Single);
     }
 
     /// <summary>
