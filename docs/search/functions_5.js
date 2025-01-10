b80e78b0a8540e08baf907c240e2bd5c339c865f
AQUATIC_TRIDENT\sande <sandervanbeurden2003@gmail.com>
2025-01-10 14:07:43 +0100

    Merge remote-tracking branch 'refs/remotes/origin/feature/330-Unhappy-Path' into feature/330-Unhappy-Path # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- docs/search/functions_5.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['fadeoutmusic_0',['FadeOutMusic',['../classSettingsManager.html#aa65144c4be7c744b43560487925f556d',1,'SettingsManager']]],
-  ['firstcycle_1',['FirstCycle',['../classGameManager.html#a8a9c87f9440395ce025bb3c2bf02eab5',1,'GameManager']]]
-];
