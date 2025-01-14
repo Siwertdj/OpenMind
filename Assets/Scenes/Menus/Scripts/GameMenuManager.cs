12890cc1acbb0cddc20c14068c4c4c841861f47e
Siwert <siwert@live.nl>
2025-01-14 16:39:01 +0100

    Erase small compiling error

--- Assets/Scenes/Menus/Scripts/GameMenuManager.cs
+++ Assets/Scenes/Menus/Scripts/GameMenuManager.cs
@@ -88,11 +88,5 @@ public class GameMenuManager : MonoBehaviour
         
         // Destroy toolbox
         Destroy(toolbox);
-        
-        /*// After that is done, we call UIManager to finish the operation.
-        FindObjectOfType<UIManager>().CloseMenu();*/
-        
-        GameManager.gm?.UnpauseGame();
-        
     }
 }
