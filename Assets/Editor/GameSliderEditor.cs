16fa0de80639aea8e3c510c817f8435be5a10726
Hille Brands <hilleannebrands@gmail.com>
2025-01-21 21:44:23 +0100

    working on sliders

--- Assets/Editor/GameSliderEditor.cs
+++ Assets/Editor/GameSliderEditor.cs
@@ -15,10 +15,10 @@ public class GameSliderEditor : Editor
     private SerializedProperty valueInfo;
     private SerializedProperty defaultValueEnabled;
     private SerializedProperty defaultValue;
-    private SerializedProperty defaultValueRef;
     private SerializedProperty sliderComponentRef;
     private SerializedProperty partitionSpace;
     private SerializedProperty partitionLinePrefab;
+    private SerializedProperty partitionsFieldRect;
 
     private bool valueTextGroup = false;
     private bool defaultValueGroup = false;
@@ -33,10 +33,10 @@ public class GameSliderEditor : Editor
         valueRounding = serializedObject.FindProperty(nameof(valueRounding));
         defaultValue = serializedObject.FindProperty(nameof(defaultValue));
         defaultValueEnabled = serializedObject.FindProperty(nameof(defaultValueEnabled));
-        defaultValueRef = serializedObject.FindProperty(nameof(defaultValueRef));
         sliderComponentRef = serializedObject.FindProperty(nameof(sliderComponentRef));
         partitionSpace = serializedObject.FindProperty(nameof(partitionSpace));
         partitionLinePrefab = serializedObject.FindProperty(nameof(partitionLinePrefab));
+        partitionsFieldRect = serializedObject.FindProperty(nameof(partitionsFieldRect));
     }
 
     public override void OnInspectorGUI()
@@ -63,7 +63,6 @@ public class GameSliderEditor : Editor
         if (defaultValueGroup)
         {
             EditorGUILayout.PropertyField(defaultValueEnabled, new GUIContent("Enable Default Value"));
-            EditorGUILayout.PropertyField(defaultValueRef, new GUIContent("Default Value Ref"));
             EditorGUILayout.PropertyField(defaultValue, new GUIContent("Default Value Suggestion"));
         }
         EditorGUILayout.EndFoldoutHeaderGroup();
@@ -71,12 +70,14 @@ public class GameSliderEditor : Editor
         partitionGroup = EditorGUILayout.BeginFoldoutHeaderGroup(partitionGroup, "Partition Line Settings");
         if (partitionGroup)
         {
+            EditorGUILayout.PropertyField(partitionSpace, new GUIContent("Partition Line Distance"));
+            EditorGUILayout.PropertyField(partitionLinePrefab, new GUIContent("Partition Line Prefab"));
         }
         EditorGUILayout.EndFoldoutHeaderGroup();
 
-        EditorGUILayout.PropertyField(partitionSpace, new GUIContent("Partition Line Distance"));
-        EditorGUILayout.PropertyField(partitionLinePrefab, new GUIContent("Partition Line Prefab"));
-
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Required Fields & References", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(partitionsFieldRect, new GUIContent("Partitions Field Rect Transform"));
 
         serializedObject.ApplyModifiedProperties();
     }
--- Assets/Scripts/UI/GameSlider.cs
+++ Assets/Scripts/UI/GameSlider.cs
@@ -17,63 +17,51 @@ public class GameSlider : MonoBehaviour
     [SerializeField] private Slider sliderComponentRef;
     [SerializeField] private float partitionSpace;
     [SerializeField] private GameObject partitionLinePrefab;
+    [SerializeField] private RectTransform partitionsFieldRect;
 
     public Slider slider { get { return sliderComponentRef; } }
 
     private void OnEnable()
     {
+
         slider.onValueChanged.AddListener(UpdateSlider);
         UpdateSlider(slider.value);
 
-        // Set default value
-        if (defaultValueEnabled)
-        {
-            // Set the default value sample to the correct position
-            defaultValueRef.transform.localPosition = new Vector2(
-                GetValuePositionOnSlider(defaultValue),
-                defaultValueRef.transform.localPosition.y);
-        }
-        else
-        {
-            defaultValueRef.gameObject.SetActive(false);
-        }
-
-        // Add partition lines
+        // Add partition lines (and default value)
         if (partitionSpace > 0)
         {
-            var parent = (RectTransform)defaultValueRef.transform.parent;
+            var parent = (RectTransform)transform;
             for (float v = slider.minValue; v < slider.maxValue; v += partitionSpace)
             {
-                Debug.Log(v);
                 // Partitions should not be placed outside of slider bounds
                 if (v <= slider.minValue || v >= slider.maxValue)
                     continue;
 
-                // Partitions should not be placed over the default value
-                if (defaultValueEnabled && v == defaultValue)
-                    continue;
-
                 // Place partition line
-                var line = Instantiate(partitionLinePrefab, parent);
+                var line = Instantiate(partitionLinePrefab, partitionsFieldRect);
                 line.transform.localPosition = new Vector2(
                     GetValuePositionOnSlider(v), 
                     line.transform.localPosition.y);
+
+                // If default value enabled, color it accordingly
+                if (defaultValueEnabled && v == defaultValue)
+                    line.GetComponent<Image>().color = Color.blue;
             }
         }
     }
 
     private float GetValuePositionOnSlider(float value)
     {
-        var fillRect = (RectTransform)defaultValueRef.transform.parent;
+        var fillRect = partitionsFieldRect;
         float totalValue = slider.maxValue - slider.minValue;
         float f = value / totalValue - slider.minValue / totalValue; // The factor of the current value
         return fillRect.rect.width * f - fillRect.rect.width / 2; // The new local x position
     }
 
-    public void UpdateSlider(float value)
+    public void UpdateSlider(float newValue)
     {
         // Adjust the value to match step precision
-        float steppedValue = Mathf.Round(value / step) * step;
+        float steppedValue = Mathf.Round(newValue / step) * step;
 
         // Update the slider's value (if you need forced snapping)
         slider.value = steppedValue;
