5557cab88f88caaee98debdc65872d1c385e2d5d
Hille Brands <hilleannebrands@gmail.com>
2025-01-22 17:22:56 +0100

    added basic robot to hint dialogue

--- Assets/Scenes/Loading/Scripts/GameManager.cs
+++ Assets/Scenes/Loading/Scripts/GameManager.cs
@@ -527,9 +527,19 @@ public class GameManager : MonoBehaviour
                 story.hintBackground, story.additionalHintBackgroundObjects[0]
                 )));
         }
+        else if (story.storyID == 2) // 2 corresponds to the AI story
+        {
+            dialogueObject = new ContentDialogueObject(
+                dialogue, null,
+                DialogueManager.dm.CreateDialogueBackground(story, null,
+                story.hintBackground, story.additionalHintBackgroundObjects[0]));
+        }
         else
         {
-            dialogueObject = new ContentDialogueObject(dialogue, null, DialogueManager.dm.CreateDialogueBackground(story, null, story.hintBackground));
+            dialogueObject = new ContentDialogueObject(
+                dialogue, null, 
+                DialogueManager.dm.CreateDialogueBackground(story, null, 
+                story.hintBackground));
         }
 
         // The gameevent here should pass the information to Dialoguemanager
