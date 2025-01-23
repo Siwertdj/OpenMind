48649d3f423b5dc5ff7cc0a6d796732d1740e06e
Joost Kootstra <joost.kootstra@gmail.com>
2025-01-23 19:47:39 +0100

    started credits, made new scene and wrote text. Started animation

---
+++ Assets/Scenes/Credits/Scripts/CreditManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private IEnumerator AnimateCredits()
+    {
+        float upperEdge = Screen.height;
+
+    }
+
+    public void CloseCredits()
+    {
+        if (SceneManager.GetSceneByName("StartScreenScene").isLoaded)
+        {
+            SceneManager.UnloadSceneAsync("CreditsScene");
+        }
+    }
+}
